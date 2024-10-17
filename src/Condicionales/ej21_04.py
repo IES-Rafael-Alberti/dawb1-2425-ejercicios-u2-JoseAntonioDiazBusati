19c937db9444de753c22f9c01f25e75aeba5bb54
JoseAntonio <jdiabus511@g.educaand.es>
2024-10-17 21:09:35 +0200

    Ejercicios 2-5

--- src/Condicionales/ej21_04.py
+++ src/Condicionales/ej21_04.py
@@ -1,3 +1,17 @@
 """
 Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
 """
+from src.Condicionales.ej21_03 import introducir_numero
+
+def comprobar_par_impar(num: int):
+    if num % 2 == 0:
+        print("Este numero es par.")
+    else:
+        print("Este numero es impar.")
+
+def main():
+    num = introducir_numero()
+    comprobar_par_impar(num)
+
+if __name__ == '__main__':
+    main()
