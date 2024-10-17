a0deb20a3963e249facbf1601a732dbcf3651644
JoseAntonio <jdiabus511@g.educaand.es>
2024-10-17 13:16:04 +0200

    Enunciados primeros 10 ejercicios

--- src/ej21_01.py
+++ src/ej21_01.py
@@ -1,3 +1,29 @@
 """
 Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
 """
+def introducir_edad():
+    return int(input("Ingrese su edad: "))
+
+def comprobar_edad()-> int:
+    age = introducir_edad()
+    assist = False
+    while not assist:
+        if age < 0:
+            print("Ingrese una edad realista!!!\n")
+            age = introducir_edad()
+        elif age > 120:
+            print("Ingrese una edad realista!!!\n")
+            age = introducir_edad()
+        else:
+            assist = True
+    return age
+
+def main():
+    edad = comprobar_edad()
+    if edad > 18:
+        print("Eres mayor de edad.")
+    else:
+        print("Eres menor de edad.")
+
+if __name__ == "__main__":
+    main()
