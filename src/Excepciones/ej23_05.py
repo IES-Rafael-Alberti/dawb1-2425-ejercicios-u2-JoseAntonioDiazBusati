7c4da9a7906db7a2d0c3406d6137a02ae294901d
JoseAntonio <jdiabus511@g.educaand.es>
2024-10-21 21:31:49 +0200

    Ejercicio 1, 2 y 3 con algo del 5

--- src/Excepciones/ej23_05.py
+++ src/Excepciones/ej23_05.py
@@ -2,3 +2,18 @@
 Escribir que solicite una contraseña, y si no coincide con la que se tiene,
 lance la excepción NameError con el mensaje, "Incorrect Password!!"
 """
+def introducir_password(mensaje):
+    password = input(mensaje)
+    return password
+
+def comparar_password(password1: str, password2: str):
+    while password1 != password2:
+
+
+def main():
+    password1 = "programacion"
+    password2 = introducir_password("Introduzca la contraseña: ")
+
+
+if __name__ == '__main__':
+    main()
