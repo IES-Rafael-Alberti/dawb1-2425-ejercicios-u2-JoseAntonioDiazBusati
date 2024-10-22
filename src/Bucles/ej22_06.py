f0173863fefb3bfe696ab4c727882e7e6dc8d225
JoseAntonio <jdiabus511@g.educaand.es>
2024-10-22 17:24:16 +0200

    Ejercicio 6 empezado

--- src/Bucles/ej22_06.py
+++ src/Bucles/ej22_06.py
@@ -8,3 +8,14 @@ pantalla un triángulo rectángulo como el de más abajo, de altura el número i
 ****
 *****
 """
+from src.Excepciones.ej23_02 import pedir_num
+
+
+def hacer_piramide(num: int):
+    piramide = "*"
+    for i in range(num):
+        print(piramide)
+
+def main():
+    num = pedir_num("Introduce un numero para hacer una piramide: ")
+    hacer_piramide(num)
